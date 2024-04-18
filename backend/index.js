a6556e73433e5a1e3d4cec8d44356b7ff49a6507
shvam0000 <shvam0000@gmail.com>
2024-04-17 22:38:03 -0400

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,16 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const noteRoutes = require('./routes/note-routes');
+
 const app = express();
 
 mongoose.connect(process.env.MONGO_DB_URI);
 
 app.use(cors());
+app.use(express.json());
+
+app.use('/notes', noteRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'API works' });
