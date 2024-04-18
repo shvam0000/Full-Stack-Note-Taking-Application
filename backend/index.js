cd4eaeb89f9baaf1724e23e9d7d4fae1d22bd4e9
shvam0000 <shvam0000@gmail.com>
2024-04-17 22:32:40 -0400

    add models and routes

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_DB_URI);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API works' });
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
