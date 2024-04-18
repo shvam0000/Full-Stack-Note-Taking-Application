ae487bde2df5cb2c85efe8e43d7bf6458cc02642
shvam0000 <shvam0000@gmail.com>
2024-04-17 23:16:03 -0400

    final code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { IoIosAddCircle } from 'react-icons/io';
 import Note from './components/note';
 
 const App = () => {
+  const [notes, setNotes] = useState();
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/notes').then((res) => {
+      console.log(res.data);
+      setNotes(res.data);
+    });
+  }, []);
+
+  const addNote = (e) => {
+    e.preventDefault();
+    console.log('Note added - ', notes);
+    axios
+      .post('http://localhost:5000/notes', {
+        title,
+        content,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setNotes([...notes, res.data]);
+        setTitle('');
+        setContent('');
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const updateNote = (id, updatedTitle, updatedContent) => {
+    axios
+      .patch(`http://localhost:5000/notes/${id}`, {
+        title: updatedTitle,
+        content: updatedContent,
+      })
+      .then((res) => {
+        console.log(res.data);
+        const updatedNotes = notes.map((note) =>
+          note._id === id
+            ? { ...note, title: updatedTitle, content: updatedContent }
+            : note
+        );
+        setNotes(updatedNotes);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const deleteNote = (id) => {
+    axios
+      .delete(`http://localhost:5000/notes/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setNotes(notes.filter((note) => note._id !== id));
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-      <h1 className="bg-yellow-400 w-screen text-xl font-medium py-4 mx-auto text-center">
+      <h1 className="w-screen bg-yellow-400 text-xl font-medium py-4 mx-auto text-center">
         Notes Keeper
       </h1>
-      <form className="py-2 shadow-xl rounded-lg px-5 w-1/3 mx-auto text-left mt-10">
+      <form
+        onSubmit={addNote}
+        className="py-2 shadow-xl rounded-lg px-5 w-1/3 mx-auto text-left mt-10">
         <input
-          className="block shadow w-full mx-auto px-2 py-2 rounded-lg"
+          className="block shadow w-full mx-auto px-2 py-2 my-2 rounded-lg"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
           type="text"
+          placeholder="Title"
         />
         <textarea
+          placeholder="Write your note here..."
+          rows={4}
           className="block shadow w-full mx-auto my-2 px-2 py-4 rounded-lg"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
           type="text"
         />
-        <button className="bg-yellow-400 text-xl px-2 rounded py-1">
-          Add Note
+
+        <button
+          className="text-yellow-400 text-3xl px-2 rounded py-1"
+          type="submit">
+          <IoIosAddCircle />
         </button>
       </form>
-      <div className="grid grid-cols-4 gap-4 py-2">
-        <Note title="First Note" content="This is my first note" />
-        <Note title="First Note" content="This is my first note" />
-        <Note title="First Note" content="This is my first note" />
+
+      <div className="grid grid-cols-4 gap-4 p-2">
+        {notes &&
+          notes.map((note) => (
+            <Note
+              key={note._id}
+              id={note._id}
+              title={note.title}
+              content={note.content}
+              deleteNote={() => deleteNote(note._id)}
+              updateNote={updateNote}
+            />
+          ))}
       </div>
     </div>
   );
--- frontend/src/components/note.jsx
+++ frontend/src/components/note.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { MdModeEditOutline } from 'react-icons/md';
+import { MdDeleteForever } from 'react-icons/md';
+import { TiTick } from 'react-icons/ti';
 
 const Note = (props) => {
   const [editMode, setEditMode] = useState(false);
@@ -6,32 +9,34 @@ const Note = (props) => {
   const [updatedContent, setUpdatedContent] = useState(props.content);
 
   const handleUpdate = () => {
-    props.updatedNote(props.id, updatedTitle, updatedContent);
+    props.updateNote(props.id, updatedTitle, updatedContent);
     setEditMode(false);
   };
 
   return (
-    <div className="border-2 px-5 py-2 w-full my-10">
+    <div className="border-2 px-5 py-2 w-full rounded my-10">
       {editMode ? (
         <div>
           <input
             value={updatedTitle}
             onChange={(e) => setUpdatedTitle(e.target.value)}
-            placeholder="Title"
-            type="text"
             className="block shadow w-full mx-auto px-2 py-2 my-2 rounded-lg"
+            required
+            type="text"
           />
           <textarea
             value={updatedContent}
             onChange={(e) => setUpdatedContent(e.target.value)}
-            placeholder="Your updated note here..."
-            type="text"
+            placeholder="Write your note here..."
+            rows={4}
             className="block shadow w-full mx-auto my-2 px-2 py-4 rounded-lg"
+            required
+            type="text"
           />
           <button
             onClick={handleUpdate}
-            className="bg-blue-400 text-xl text-white px-2 py-1 rounded-lg">
-            Edit
+            className="text-blue-400 text-3xl px-2 py-1">
+            <TiTick />
           </button>
         </div>
       ) : (
@@ -43,11 +48,13 @@ const Note = (props) => {
           <div className="flex justify-center items-center">
             <button
               onClick={() => setEditMode(true)}
-              className="py-1 my-2 px-3 bg-blue-400 text-xl text-white rounded-lg mx-2">
-              Edit
+              className="py-1 my-2 px-3 text-blue-400 text-3xl rounded-lg mx-2">
+              <MdModeEditOutline />
             </button>
-            <button className="py-1 my-2 px-3 bg-red-600 text-xl text-white rounded-lg mx-2">
-              Delete
+            <button
+              onClick={props.deleteNote}
+              className="py-1 my-2 px-3 text-red-600 text-3xl rounded-lg mx-2">
+              <MdDeleteForever />
             </button>
           </div>
         </div>
